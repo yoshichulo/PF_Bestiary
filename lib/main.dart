86352c97c91a762125f5034694c54b6ffd0c2829
Jose Angel <joseangelyoshi@gmail.com>
2020-03-09 12:18:14 +0100

    Implemented top bar

--- lib/main.dart
+++ lib/main.dart
@@ -21,43 +21,100 @@ class TopSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [Colors.blue, Colors.yellow],
-            ),
-          ),
-          margin: EdgeInsets.all(15.0),
-          child: Text(
-            'Aboleth',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        )
+        MobNameWidget(),
+        MobLogoWidget(),
+        Spacer(),
+        MobCRWidget(),
       ],
     );
   }
 }
-/*
-class Separator extends StatelessWidget {
+
+class MobNameWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            height: 1,
-            color: Colors.black87,
-            margin: const EdgeInsets.only(left: 10.0, right: 10.0),
+    return Container(
+      padding: EdgeInsets.only(top: 12, bottom: 6, left: 30, right: 30),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.only(
+          bottomRight: Radius.circular(22),
+        ),
+        gradient: LinearGradient(
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+          colors: [
+            Color.fromARGB(255, 220, 36, 48),
+            Color.fromARGB(255, 150, 68, 191)
+          ],
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'Aboleth',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 34,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-        )
-      ],
+          Text(
+            '9,843,292 XP',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+            ),
+          ),
+        ],
+      ),
     );
   }
-} */
+}
+
+class MobLogoWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(8),
+      child: FittedBox(
+        fit: BoxFit.fill,
+        child: Image.asset('assets/images/aberration_logo.png'),
+      ),
+    );
+  }
+}
+
+class MobCRWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+          minHeight: 50, minWidth: 50, maxHeight: 60, maxWidth: 60),
+      child: Container(
+        padding: EdgeInsets.all(8),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color.fromARGB(255, 220, 36, 48),
+              Color.fromARGB(255, 150, 68, 191)
+            ],
+          ),
+        ),
+        child: Text(
+          '151',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 34,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
